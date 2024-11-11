5bbaf52f54d4bab442d215fa7f396ec34fdcf6cf
axwii <axwi0001@stud.kea.dk>
2024-11-11 15:23:41 +0100

    Enhance Next.js configuration and add new pages; implement product fetching and navigation

---
+++ src/app/detaljer/[id]/page.js
@@ -0,0 +1,8 @@
+const Page = async ({ params }) => {
+  const id = (await params).id;
+  let response = await fetch(`https://dummyjson.com/products/${id}`);
+  let product = await response.json();
+  return <div>{product.title}</div>;
+};
+
+export default Page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  let response = await fetch("https://dummyjson.com/products");
+  let data = await response.json();
   return (
-    <div className="min-h-screen p-8 pb-20 sm:p-20">
-      <main className="flex flex-col gap-8 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
+    <div className="grid grid-cols-[1fr_1fr_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+      {data.products.map((product) => {
+        return (
+          <div
+            key={product.id}
+            className="p-3 rounded-lg shadow-sm bg-amber-200"
+          >
+            <Image
+              src={product.thumbnail}
+              width={250}
+              height={250}
+              alt={product.title}
+            />
+            <Link href={`/detaljer/${product.id}`}>{product.title}</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
